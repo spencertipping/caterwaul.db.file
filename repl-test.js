feee188eb1bc351d3221c100b36fadd0c258800e
Spencer Tipping <spencer@spencertipping.com>
2010-11-08 08:28:08 -0700

    Using test-db as the REPL database

--- repl-test.js
+++ repl-test.js
@@ -1,1 +1,1 @@
-require('repl').start('caterwaul.db.file> ').context.db = caterwaul.clone('db.file').db.file('db');
+require('repl').start('caterwaul.db.file> ').context.db = caterwaul.clone('db.file').db.file('test-db');
